9c920cfd10e5bf100def9efb615315b9e85c75c8
Emily Nixon <123026549+enixon01@users.noreply.github.com>
2023-04-28 11:42:40 -0400

    Add files via upload

---
+++ Quiz121.py
@@ -0,0 +1,50 @@
+#Emily Nixon
+#Problem 1
+file1=open("lines.txt","w")
+line1 = "I am so tired. \n"
+file1.write(line1)
+line2 = "I have had two exams today.\n"
+file1.write(line2)
+line3 = "I feel like I'm blanking on how to do this.\n"
+file1.write(line3)
+line4 = "I hate chemistry. Why am I minoring in chemistry.\n"
+file1.write(line4)
+line5="I'm going to take the fattest nap after this.\n"
+file1.write(line5)
+file1.close()
+
+#Problem 2
+file1=open("lines.txt","r")
+for line in file1:
+    print(line)
+
+#Problem 3
+import dbm
+houses = dbm.open("houses.txt","c")
+["house1.png" == "a 2 bed, 3 bath Victorian House"]
+["house2.png" == "a 1 bed, 2 bath contemporary home"]
+["house3.png" == "a 4 bed, 2 bath midcentury modern home"]
+["houses4.png" == "a 2 bedroom apartment"]
+["houses5.png" == "a 6 bedroom, 3 bath mini-mansion"]
+["houses6.png" == "a 1 bedroom, 1 bathroom starter home"]
+
+#Problem 4
+["house1.png" == "a 2 bed, 3 bath Victorian House"]
+["house2.png" == "a 1 bed, 2 bath contemporary home"]
+["house3.png" == "a 4 bed, 2 bath midcentury modern home"]
+["houses4.png" == "a 2 bedroom apartment"]
+["houses5.png" == "a 6 bedroom, 3 bath mini-mansion"]
+["houses6.png" == "a 1 bedroom, 1 bathroom starter home"]
+["houses5.png" == "a 2 bedroom home"]
+["houses6.png" == "a 3 bedroom home with no bathroom"]
+
+for n.png in houses:
+    print(n)
+    
+
+
+             
+
+
+
+    
